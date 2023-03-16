a7616c4f54557a11c19f04383852bd937f89e9ab
Tibovc <43603205+TiboVanCraenenbroeck@users.noreply.github.com>
2023-03-16 20:35:26 +0100

    Agent

---
+++ Q_learning_agent.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+class QLearningAgent:
+    def __init__(self, nr_states, alpha, gamma, exploration_rate, decay):
+        self.possible_actions= [0, 1]
+
+        self.nr_states = nr_states
+        self.nr_actions = len(self.possible_actions)
+
+        self.alpha = alpha
+        self.gamma = gamma
+        self.exploration_rate = exploration_rate
+        self.decay = decay
+
+
+        self.q_table = np.zeros((10, 10, 10, 10, self.nr_actions))
+    
+
+    def compute_action(self, state):
+        self.state = state
+        cart_position, cart_velocity, pole_angle, pole_velocity_at_tip = self.compute_discrete_state(state)
+        self.exploration_rate = self.exploration_rate * self.decay
+        if np.random.uniform() <= self.exploration_rate:
+            # Exploration
+            self.action = np.random.choice(self.possible_actions, 1)[0]
+        else:
+            # Exploitation
+            self.action = np.argmax(self.q_table[cart_position, cart_velocity, pole_angle, pole_velocity_at_tip, :])
+        return self.action
+    
+    def update_qtable(self, state, new_state, reward):
+        cart_position, cart_velocity, pole_angle, pole_velocity_at_tip = self.compute_discrete_state(state)
+        cart_position_n, cart_velocity_n, pole_angle_n, pole_velocity_at_tip_n = self.compute_discrete_state(new_state)
+        self.q_table[cart_position, cart_velocity, pole_angle, pole_velocity_at_tip, self.action] = (1-self.alpha) * self.q_table[cart_position, cart_velocity, pole_angle, pole_velocity_at_tip, self.action] + self.alpha * (reward + self.gamma * self.q_table[cart_position_n, cart_velocity_n, pole_angle_n, pole_velocity_at_tip_n, np.argmax(self.q_table[cart_position_n, cart_velocity_n, pole_angle_n, pole_velocity_at_tip_n, :])])
+    
+
+    def change_exploration(self):
+        self.exploration_rate = self.exploration_rate * self.decay
+    
+    def create_bins(self, possible_states, nr_bins):
+        nr_bins -= 1
+        self.bins = np.zeros((len(possible_states), nr_bins))
+        for i, possible_state in enumerate(possible_states):
+            self.bins[i] = np.linspace(possible_state[0], possible_state[1], nr_bins)
+        
+    
+    def compute_discrete_state(self, states):
+        discrete_states: list = []
+        for i, state in enumerate(states):
+            discrete_states.append(np.digitize(state, self.bins[i]))
+        return discrete_states
+        
---
+++ oef_01.py
@@ -0,0 +1,81 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import gym
+
+from Q_learning_agent import QLearningAgent
+
+# Omgeving aanmaken
+env_not_human = gym.make('CartPole-v0')
+env_human = gym.make('CartPole-v0', render_mode = "human")
+
+
+alpha: float = 0.1
+gamma: float = 0.9
+decay: float = 0.9999
+nr_states: int = 10**4
+exploration_rate: float = 0.999
+
+
+# Agent aanmaken
+agent: QLearningAgent = QLearningAgent(nr_states, alpha, gamma, exploration_rate, decay)
+possible_states = [[-2.4, 2.4], [-4,4], [-0.4181, 0.4181], [-4, 4]]
+agent.create_bins(possible_states, 10)
+
+
+aantal_episodes: int = 1000
+aantal_stappen_per_episode: list = []
+max_aantal_stappen_per_episode: int = 200
+
+for episode in range(aantal_episodes):
+    agent.change_exploration()
+    aantal_stappen: int = 0
+
+    # Omgeving resetten
+    env = env_human if episode % 100 == 0 else env_not_human
+    observatie, _ = env.reset()
+    done = False
+
+    # Iteratie
+    while not done:
+        aantal_stappen += 1
+        # Actie kiezen
+        actie = agent.compute_action(observatie)
+
+        # Actie uitvoeren
+        observatie_nu, reward, terminated, truncated, _ = env.step(actie)
+
+        if terminated or truncated:
+            done = True
+            aantal_stappen_per_episode.append(aantal_stappen)
+
+        # Reward aanpassen
+        if aantal_stappen < max_aantal_stappen_per_episode and done is True:
+            reward = -100
+
+        # Agent trainen
+        agent.update_qtable(observatie, observatie_nu, reward)
+        observatie = observatie_nu
+
+        
+
+max_aantal_stappen: int = max(aantal_stappen_per_episode)
+min_aantal_stappen: int = min(aantal_stappen_per_episode)
+gemiddeld_aantal_stappen = sum(aantal_stappen_per_episode)/len(aantal_stappen_per_episode)
+
+print(f"max aantal stappen: {max_aantal_stappen}")
+print(f"min aantal stappen: {min_aantal_stappen}")
+print(f"gemiddeld aantal stappen: {gemiddeld_aantal_stappen}")
+
+test = pd.DataFrame(data={"aantal_episodes": range(aantal_episodes), "aantal_stappen_per_episode": aantal_stappen_per_episode})
+
+plt.bar(range(aantal_episodes), aantal_stappen_per_episode, label="Aantal stappen", width=1)
+plt.title("Aantal stappen per episode")
+plt.xlabel("Aantal episodes")
+plt.ylabel("Aantal stappen")
+plt.show()
+
+sns.boxplot(data=aantal_stappen_per_episode)
+plt.show()
+
+env.close()
